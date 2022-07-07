b7b238971591237b7e42be439a8c618703e66840
chuluq <choirulchuluq10@gmail.com>
2022-07-07 11:27:39 +0700

    add booking section

--- pages/index.js
+++ pages/index.js
@@ -116,6 +116,34 @@ export default function HomePage() {
           <button className="btn">more about us</button>
         </div>
       </section>
+
+      <section className="booking">
+        <div className="booking-text">
+          <h3>Book a table</h3>
+          <p>Do it in advance</p>
+        </div>
+        <form className="booking-form">
+          <div className="text-field">
+            <label htmlFor="name">Name*</label>
+            <input type="text" placeholder="Your name" />
+          </div>
+          <div className="text-field">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" placeholder="Your last name" />
+          </div>
+          <div className="text-field">
+            <label htmlFor="email">Email*</label>
+            <input type="email" placeholder="Your email address" />
+          </div>
+          <div className="text-field">
+            <label htmlFor="message">Message*</label>
+            <textarea placeholder="Enter your message" rows="3" />
+          </div>
+        </form>
+        <div className="booking-cta">
+          <button className="btn">Book a table</button>
+        </div>
+      </section>
     </Layout>
   );
 }
